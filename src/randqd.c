b21c076cc6aa37b8e161675ed8c94b5b71aa2116
Alessandro Cudazzo <alessandro@cudazzo.com>
2019-05-17 12:04:33 +0200

    ADDED randqd lib v0.1.0

---
+++ src/randqd.c
@@ -0,0 +1,38 @@
+#include "randqd.h"
+
+/*
+ * Copyright (c) 2019 Alessandro Cudazzo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+
+
+/**
+ * Return a random sample in the range [0, 2^32 - 1].
+ * look in  "Numerical Recipes in C" Second Edition 
+ * for the numbers explanation
+ *
+ * \param       x   PRNG state
+ * \return      Random number between [0, 2^32 - 1]
+ */
+inline uint32_t randqd_uint32(uint32_t x)
+{
+    return (uint32_t) (1664525UL * x + 1013904223UL);
+}
---
+++ src/randqd.h
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2019 Alessandro Cudazzo
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See `LICENCE` file for details.
+ */
+
+#ifndef RANDQD_H
+#define RANDQD_H
+
+#include <stdint.h>
+
+#define RANDQD_VERSION "0.1.0"
+
+
+/*
+*********************************************************************
+*             A better way to generate a random number              *
+*********************************************************************
+*
+*   PRNG (Pseudo-Random Number Generators):
+*   First, and perhaps most important: be very, very suspicious of a 
+*   system-supplied rand(). System-supplied rand()s are almost always 
+*   linear congruential generators that will eventually repeat itself,
+*   with a period.
+*
+*   A better way to generate a random number is th "Quick and Dirty" 
+*   Generators, described in  "Numerical Recipes in C" Second Edition 
+*   at page 284, relies on 32-bit arithmetic.
+*   It's works on a 32bit machine, so we need to use uint32_t.
+*/
+
+
+/**
+ * Returns a random value within the range [0, 2^32 - 1]
+ *
+ * \param       x   PRNG state
+ * \return      Random number between [0, 2^32 - 1]
+ */
+uint32_t randqd_uint32(uint32_t x);
+
+
+
+#endif
