53e78cdd5fea1b25e3dfacd8cc1cbbc20661b6c5
Alessandro Cudazzo <alessandro@cudazzo.com>
2019-05-21 17:07:26 +0200

    update lib to v2.0.0, now has two PRNG
    add a new method to generate a PRN: "suspenders-and-belt,full-body-armor, never-any-doubt" generator  laying on 64bit and respect all the constraints defined in "Numerical Recipes" Third Edition at page 342.

--- src/randq.c
+++ src/randq.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 
-#include "randqd.h"
+#include "randq.h"
 
 /*
  * Copyright (c) 2019 Alessandro Cudazzo
@@ -24,8 +24,13 @@
  * IN THE SOFTWARE.
  */
 
-bool is_rseed_init = false;
+// Variable for "quick and dirt" PRNG
 static uint32_t rqd_seed = 0UL;
+
+// Variables for "64bit" PRNG
+static bool is_seed_init = false;
+static uint64_t default_rq64_seed = 1ULL;
+static uint64_t u,v,w;
 
 
 /**
@@ -60,3 +65,67 @@ double randqd_double()
 {
     return randqd_uint32() / (double) (1L << 32);
 }
+
+/**
+ * Init the First Seed
+ *
+ * \start_seed      Started seed
+ */
+void srandq64(uint64_t start_seed)
+{
+    is_seed_init = true;
+    v = 4101842887655102017LL;
+    w = 1;
+	u = start_seed ^ v; 
+    randq64_uint64();
+	v = u; 
+    randq64_uint64();
+	w = v; 
+    randq64_uint64();
+}
+
+/**
+ * Return a random sample in the range [0, 2^64 - 1].
+ * look in  "Numerical Recipes in C" Third Edition 
+ * for the numbers explanation
+ *
+ * \return      Random number between [0, 2^64 - 1]
+ */
+uint64_t randq64_uint64()
+{    
+    if (!is_seed_init)
+        srandq64(default_rq64_seed);
+    
+	u = u * 2862933555777941757LL + 7046029254386353087LL;
+	v ^= v >> 17; 
+    v ^= v << 31; 
+    v ^= v >> 8;
+	w = 4294957665U*(w & 0xffffffff) + (w >> 32);
+	uint64_t x = u ^ (u << 21); 
+    x ^= x >> 35; 
+    x ^= x << 4;
+	return (x + v) ^ w;
+}
+
+/**
+ * Return a random sample in the range [0, 2^32 - 1].
+ * look in  "Numerical Recipes in C" Third Edition 
+ * for the numbers explanation
+ *
+ * \return      Random number between [0, 2^32 - 1]
+ */
+uint32_t randq64_uint32()
+{    
+    return (uint32_t) randq64_uint64();
+}
+
+/**
+ * Return a random number in the range [0, 1)
+ *
+ * \return      Random number between [0, 1)
+ */
+double randq64_double()
+{
+    return 5.42101086242752217E-20 * randq64_uint64();
+}
+
